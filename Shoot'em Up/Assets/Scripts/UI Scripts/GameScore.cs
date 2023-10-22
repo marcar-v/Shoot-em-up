e06b23e32f4a8e4c5db652dee64b4c5a9f62b9be
marcar-v <vmarcar95@gmail.com>
2023-10-22 13:09:30 +0200

    Highscore added

--- Shoot'em Up/Assets/Scripts/UI Scripts/GameScore.cs
+++ Shoot'em Up/Assets/Scripts/UI Scripts/GameScore.cs
@@ -9,6 +9,8 @@ public class GameScore : MonoBehaviour
     TextMeshProUGUI scoreText;
     int score;
 
+    [SerializeField] TextMeshProUGUI highscore;
+
     public int Score 
     { 
         get
@@ -19,6 +21,7 @@ public class GameScore : MonoBehaviour
         {
             score = value;
             UpdateScoreTextUI();
+            UpdateHighscoreTextUI();
         }
     
     }
@@ -26,11 +29,23 @@ public class GameScore : MonoBehaviour
     private void Start()
     {
         scoreText = GetComponent<TextMeshProUGUI>();
+
+        highscore.text = PlayerPrefs.GetInt("Highscore", 0).ToString();
     }
 
     private void UpdateScoreTextUI()
     {
         string scoreStr = string.Format("{0:0000000}", score);
         scoreText.text = scoreStr;
+
+    }
+
+    public void UpdateHighscoreTextUI()
+    {
+        if(score > PlayerPrefs.GetInt("Highscore", 0))
+        {
+            PlayerPrefs.SetInt("Highscore", score);
+            highscore.text = score.ToString();
+        }
     }
 }
--- Shoot'em Up/Assets/Scripts/UI Scripts/ScrollingBackground.cs
+++ Shoot'em Up/Assets/Scripts/UI Scripts/ScrollingBackground.cs
@@ -1,9 +1,7 @@
 using UnityEngine;
 
-
 public class ScrollingBackground : MonoBehaviour
 {
-
     [SerializeField] float scrollSpeed = 0.5f;
     private Vector3 startPosition;
 
